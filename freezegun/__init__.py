035bdbf82af3a4dd5e0b68052ab91b128bb9cb23
Steve Pulec <spulec@gmail.com>
2013-01-16 16:18:10 -0500

    0.1.0

--- freezegun/__init__.py
+++ freezegun/__init__.py
@@ -10,7 +10,7 @@ freezegun
 """
 
 __title__ = 'freezegun'
-__version__ = '0.0.8'
+__version__ = '0.1.0'
 __author__ = 'Steve Pulec'
 __license__ = 'Apache License 2.0'
 __copyright__ = 'Copyright 2012 Steve Pulec'
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='freezegun',
-    version='0.0.9',
+    version='0.1.0',
     description='Let your Python tests travel through time',
     author='Steve Pulec',
     author_email='spulec@gmail',
