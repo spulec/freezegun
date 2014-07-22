367bcb62ef50bd96db4e773a9bc80198945c7628
Haydn Dufrene <haydn.dufrene@affirm.com>
2014-07-21 18:49:35 -0700

    setup: bump to 0.1.118.affirm

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='freezegun',
-    version='0.1.18',
+    version='0.1.18.affirm',
     description='Let your Python tests travel through time',
     author='Steve Pulec',
     author_email='spulec@gmail',
