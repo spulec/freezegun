ba9fbec7bf785d1e16e8c6340a3be2a5307b2815
Steve Pulec <spulec@gmail.com>
2015-04-09 22:49:20 -0400

    0.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='freezegun',
-    version='0.3.1',
+    version='0.3.2',
     description='Let your Python tests travel through time',
     author='Steve Pulec',
     author_email='spulec@gmail',
