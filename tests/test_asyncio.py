c2c4a6d5d3f2ea9dac0a123807268bac1b59b25e
Jonas Obrist <jonas.obrist@hde.co.jp>
2018-01-05 10:59:10 +0900

    fixed syntax error due to async def on older pythons

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,4 +1,5 @@
 import datetime
+from textwrap import dedent
 
 from freezegun import freeze_time
 
@@ -10,8 +11,17 @@ except ImportError:
 
 if asyncio:
     def test_time_freeze_coroutine():
+        @asyncio.coroutine
         @freeze_time('1970-01-01')
-        async def frozen_coroutine():
+        def frozen_coroutine():
             assert datetime.date.today() == datetime.date(1970, 1, 1)
 
         asyncio.get_event_loop().run_until_complete(frozen_coroutine())
+
+    exec(dedent('''
+    def test_time_freeze_async_def():
+        @freeze_time('1970-01-01')
+        async def frozen_coroutine():
+            assert datetime.date.today() == datetime.date(1970, 1, 1)
+        asyncio.get_event_loop().run_until_complete(frozen_coroutine())
+    '''))
