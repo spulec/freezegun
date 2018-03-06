47c38393c2a8ef4e16e62df794c26e57b537c76d
Steve Pulec <spulec@gmail.com>
2018-03-05 21:50:19 -0500

    Drop py3.7 for now due to uuid1 issues.

--- setup.py
+++ setup.py
@@ -35,6 +35,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
 )
