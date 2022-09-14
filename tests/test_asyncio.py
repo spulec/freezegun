96c2741b3c41133cb06a45d82b10a9c3c18cdb55
Marcin Sulikowski <marcin.k.sulikowski@gmail.com>
2022-09-14 18:00:22 +0200

    Clean up asyncio tests
    We finish the cleanup of `test_asyncio.py` done in c60dd14b05f09c482d4
    and in 204647b305 by removing remnants of support of Pythons with no
    `asyncio` module because all Python interpreters supported by freezegun
    already support `asyncio`.

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,15 +1,10 @@
 import asyncio
 import datetime
-from textwrap import dedent
-from unittest import SkipTest
 
 from freezegun import freeze_time
 
 
-def test_time_freeze_coroutine():
-    if not asyncio:
-        raise SkipTest('asyncio required')
-
+def test_datetime_in_coroutine():
     @freeze_time('1970-01-01')
     async def frozen_coroutine():
         assert datetime.date.today() == datetime.date(1970, 1, 1)
