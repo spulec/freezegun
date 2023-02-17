3d5d60b461381271d0cffcd343b6459174f65141
Moshe Jacobs <micromoses@gmail.com>
2023-02-17 10:47:30 +0200

    Handle callable staticmethods (py3.10);
    Up to python 3.10, staticmethods were not callable - so they were skipped by the condition.
    Since python 3.10, staticmethods ARE callable - so adding the `isinstance(v, staticmethod)`
    to the same `if` statement would consistently handle staticmethods across python
    versions <=3.9 and >= 3.10.
    This solves for:
    ```
    self = <tests.test_datetimes.Tester object at 0x1073dc5e0>
    
        def test_class_decorator_respects_staticmethod(self):
    >       assert self.helper() == datetime.date(2012, 1, 14)
    
    tests/test_datetimes.py:453:
    ...
    >               result = func(*args, **kwargs)
    E               TypeError: Tester.helper() takes 0 positional arguments but 1 was given
    
    freezegun/api.py:809: TypeError
    ```

--- freezegun/api.py
+++ freezegun/api.py
@@ -619,7 +619,7 @@ class _freeze_time:
                         continue
                     seen.add(attr)
 
-                    if not callable(attr_value) or inspect.isclass(attr_value):
+                    if not callable(attr_value) or inspect.isclass(attr_value) or isinstance(attr_value, staticmethod):
                         continue
 
                     try:
