a6080557cbcbe4891843934c1334430bb1c5ead7
Bert Blommers <info@bertblommers.nl>
2025-08-09 10:16:31 +0000

    Allow parametrized arguments with name 'func'

--- tests/test_class_decorator.py
+++ tests/test_class_decorator.py
@@ -15,6 +15,10 @@ class TestClassDecoratorWithFixture:
     def yield_ff(self) -> Iterator[datetime]:
         yield datetime.now()
 
+    @pytest.fixture
+    def func(self) -> Iterator[datetime]:
+        yield datetime.now()
+
     def test_with_fixture(self, ff: datetime) -> None:
         assert ff == FakeDatetime(2022, 10, 1, 0, 0)
         assert datetime.now() == FakeDatetime(2022, 10, 1, 0, 0)
@@ -25,3 +29,28 @@ class TestClassDecoratorWithFixture:
     def test_with_yield_fixture(self, yield_ff: datetime) -> None:
         assert yield_ff == FakeDatetime(2022, 10, 1, 0, 0)
         assert datetime.now() == FakeDatetime(2022, 10, 1, 0, 0)
+
+    def test_with_func_fixture(self, func: datetime) -> None:
+        assert func == FakeDatetime(2022, 10, 1, 0, 0)
+        assert datetime.now() == FakeDatetime(2022, 10, 1, 0, 0)
+
+
+@pytest.mark.parametrize("func", ("a", "b"))
+@freeze_time(datetime.now())
+def test_decorator_with_argument_named_func(func: str) -> None:
+    """Verify that we can pass an argument called 'func'"""
+    assert func in ("a", "b")
+
+
+@pytest.mark.parametrize("arg", ("a", "b"))
+@freeze_time(datetime.now())
+def test_freezegun_with_argument_named_arg(arg: str) -> None:
+    """Verify that we can pass an argument called 'arg'"""
+    assert arg in ("a", "b")
+
+
+@freeze_time(datetime.now())
+@pytest.mark.parametrize("func", ("a", "b"))
+def test_freezegun_decorator_first_parametrize_second(func: str) -> None:
+    """Verify that we can pass the parametrized function into freezegun"""
+    assert func in ("a", "b")
